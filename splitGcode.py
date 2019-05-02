e16ee1563bc2e3b7a55f95456f5a6c0561065e55
Phuc <nguyenxuanphuc6794@gmail.com>
2019-05-02 13:55:35 +0700

    Update splitGcode.py

--- splitGcode.py
+++ splitGcode.py
@@ -2,8 +2,8 @@ def split():
         sourcefile = "Gcode/box300x100.gcode"
         file1 = "Gcode/one.gcode"
         file2 = "Gcode/two.gcode"
-
-        f = open(sourcefile, "r")
+        startcode = "Gcode/startcode.gcode"
+        
         #data = f.readline()
         flag = 0
         f1 = open(file1, "w")
@@ -14,6 +14,14 @@ def split():
         f2.close
         f1 = open(file1, "a")
         f2 = open(file2, "a")
+        
+        f = open(startcode, "r")
+        for x in f:
+                f1.write(x)
+                f2.write(x)
+        f.close
+
+        f = open(sourcefile, "r")
         for x in f:
                 if x[0:2] == 'T0':
                         #switch to copy to file box1
@@ -24,17 +32,21 @@ def split():
                 else:
                         if flag == 1:
                                 # copy to box 1
-                                
                                 f1.write(x)
-                                print(x)
+                                if x[0:4] == "G1 Z":
+                                        f2.write(x)
+                                #print(x)
                                 #f1.close
                         elif flag == 2:
                                 # copy to box2
                                 #f2 = open(file2, "a")
                                 f2.write(x)
+                                if x[0:4] == "G1 Z":
+                                        f1.write(x)
                                 #f2.close
                         else:
                                 pass
         f1.close
         f2.close
+        f.close
 split()
